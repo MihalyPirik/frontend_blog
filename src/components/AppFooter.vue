e7c2a08e54c3c6731a94cf9ba92ca3c8fc367156
Mihaly Pirik <misipirik@gmail.com>
2023-12-14 11:31:09 +0100

    init

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="container-fluid p-0 bg-success-subtle">
+        <p class="text-center text-white py-2">&copy;2023 - PM</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+    <div class="container">
+        OK
+    </div>
+</template>
